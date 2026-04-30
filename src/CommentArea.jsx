a8e82954d1e4844c54ded536f8b58b029723b961
PaoloF16 <paolo.cafis@gmail.com>
2026-04-30 14:43:30 +0200

    changes

--- src/CommentArea.jsx
+++ src/CommentArea.jsx
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react"
-import { FloatingLabel, Form, ListGroup } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { FloatingLabel, Form, ListGroup } from "react-bootstrap";
 
-const ApiUrl = "https://striveschool-api.herokuapp.com/api/comments/"
-const Token = "Bearer TU_TOKEN"
+const ApiUrl = "https://striveschool-api.herokuapp.com/api/comments/";
+const Token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTFhYzczOWY4NzAwMTU3YWIwODgiLCJpYXQiOjE3NzY0MTEwNTIsImV4cCI6MTc3NzYyMDY1Mn0.TlevMKTw6sR2qLolKy_AF0czZscRN7wRkVPwPX79Vq4";
 
 const CommentArea = ({ asin }) => {
-  const [comments, setComments] = useState([])
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     fetch(`${ApiUrl}${asin}`, {
       headers: {
-        Authorization: Token
-      }
+        Authorization: Token,asdsaddad
+      },
     })
-      .then(res => res.json())
-      .then(data => setComments(data))
-  }, [asin])
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, [asin]);
 
   return (
     <>
       <ListGroup>
-        {comments.map(c => (
-          <ListGroup.Item key={c._id}>
-            {c.comment}
-          </ListGroup.Item>
+        {comments.map((c) => (
+          <ListGroup.Item key={c._id}>{c.comment}</ListGroup.Item>
         ))}
       </ListGroup>
 
@@ -35,7 +34,7 @@ const CommentArea = ({ asin }) => {
         />
       </FloatingLabel>
     </>
-  )
-}
+  );
+};
 
-export default CommentArea
+export default CommentArea;
