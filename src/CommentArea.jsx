ba014ccc4c67ba8a989c8e097ac224efa0de44f6
PaoloF16 <paolo.cafis@gmail.com>
2026-04-30 14:37:03 +0200

    first item

--- src/CommentArea.jsx
+++ src/CommentArea.jsx
@@ -1,17 +1,41 @@
-import { FloatingLabel, Form } from "react-bootstrap";
+import { useEffect, useState } from "react"
+import { FloatingLabel, Form, ListGroup } from "react-bootstrap"
 
-const CommentArea = ()=>{
-    return (
-        <>
-         <FloatingLabel controlId="floatingTextarea2" label="Comments">
+const ApiUrl = "https://striveschool-api.herokuapp.com/api/comments/"
+const Token = "Bearer TU_TOKEN"
+
+const CommentArea = ({ asin }) => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    fetch(`${ApiUrl}${asin}`, {
+      headers: {
+        Authorization: Token
+      }
+    })
+      .then(res => res.json())
+      .then(data => setComments(data))
+  }, [asin])
+
+  return (
+    <>
+      <ListGroup>
+        {comments.map(c => (
+          <ListGroup.Item key={c._id}>
+            {c.comment}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+
+      <FloatingLabel controlId="floatingTextarea2" label="Comments">
         <Form.Control
           as="textarea"
           placeholder="Leave a comment here"
-          style={{ height: '100px' }}
-          />
+          style={{ height: "100px" }}
+        />
       </FloatingLabel>
-          </>
-    )
+    </>
+  )
 }
 
 export default CommentArea
--- src/SingleBook.jsx
+++ src/SingleBook.jsx
@@ -1,15 +1,17 @@
 import { Card } from "react-bootstrap"
 import CommentArea from "./CommentArea"
+import {useState} from "react"
 
 const SingleBook = ({ book }) => {
-
+const [selected, SetSelected] = useState(false)/* li do i valore false perche poi sara cambiato dentro il component */
   return (
-    <Card>
-      <Card.Img variant="top" src={book.img} />
+    <Card >
+      <Card.Img variant="top" src={book.img} onClick={()=> SetSelected(!selected)} />
       <Card.Body>
         <Card.Title>{book.title}</Card.Title>
       </Card.Body>
-      <CommentArea></CommentArea>
+      {selected && <CommentArea asin={book.asin} />}
+      
     </Card>
   )
 }
