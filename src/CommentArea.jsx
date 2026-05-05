63d6b2b77c4fb60eb5e63398a4dfcb1961704e99
PaoloF16 <paolo.cafis@gmail.com>
2026-05-05 14:01:27 +0200

    finished

--- src/CommentArea.jsx
+++ src/CommentArea.jsx
@@ -1,94 +1,69 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { Alert, Spinner, ListGroup } from "react-bootstrap";
+const ApiUrl = "https://striveschool-api.herokuapp.com/api/comments/";
+const Token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTFhYzczOWY4NzAwMTU3YWIwODgiLCJpYXQiOjE3NzY0MTEwNTIsImV4cCI6MTc3NzYyMDY1Mn0.TlevMKTw6sR2qLolKy_AF0czZscRN7wRkVPwPX79Vq4";
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: false,
-    isError: false,
-  };
+const CommentArea = (props) => {
+  const [comment, SetComment] = useState([]);
+  const [loading, SetLoading] = useState(false);
+  const [isError, SetIsError] = useState(false);
 
-  fetchComments = () => {
-    // Si no hay libro seleccionado, no hacemos fetch
-    if (!this.props.asin) {
-      return;
-    }
-
-    this.setState({
-      isLoading: true,
-      isError: false,
-    });
-
-    fetch(
-      "https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin,
-      {
+  const getComments = async () => {
+    SetLoading(true);
+    SetIsError(false);
+    try {
+      const response = await fetch(ApiUrl + props.asin, {
+        method: "GET",
         headers: {
-          Authorization: "TU_TOKEN_AQUI",
+          Authorization: Token,
         },
-      },
-    )
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Error al recuperar comentarios");
-        }
-      })
-      .then((comments) => {
-        this.setState({
-          comments: comments,
-          isLoading: false,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({
-          isLoading: false,
-          isError: true,
-        });
       });
-  };
-
-  componentDidUpdate(prevProps) {
-    // Solo hacemos fetch cuando cambia el asin seleccionado
-    if (prevProps.asin !== this.props.asin) {
-      this.fetchComments();
+      if (!response.ok) {
+        throw new Error("Error fetching comments");
+      }
+      const data = await response.json();
+      SetComment(data);
+    } catch (error) {
+      SetIsError(true);
+    } finally {
+      SetLoading(false);
     }
-  }
+  };
+  useEffect(() => {
+    if (props.asin) {
+      getComments();
+    }
+  }, [props.asin]);
+  return (
+    <div className="mt-4">
+      <h4>Comentarios</h4>
 
-  render() {
-    return (
-      <div className="mt-4">
-        <h4>Comentarios</h4>
+      {!props.asin && (
+        <Alert variant="info">Selecciona un libro para ver comentarios</Alert>
+      )}
 
-        {!this.props.asin && (
-          <Alert variant="info">Selecciona un libro para ver comentarios</Alert>
-        )}
+      {loading && <Spinner animation="border" />}
 
-        {this.state.isLoading && <Spinner animation="border" />}
+      {isError && <Alert variant="danger">Error al cargar comentarios</Alert>}
 
-        {this.state.isError && (
-          <Alert variant="danger">Error al cargar comentarios</Alert>
-        )}
-
-        {!this.state.isLoading && this.props.asin && (
-          <ListGroup>
-            {this.state.comments.map((comment) => {
-              return (
-                <ListGroup.Item key={comment._id}>
-                  <strong>{comment.author}</strong>
-                  <br />
-                  {comment.comment}
-                  <br />
-                  Valoración: {comment.rate}
-                </ListGroup.Item>
-              );
-            })}
-          </ListGroup>
-        )}
-      </div>
-    );
-  }
-}
+      {!loading && props.asin && (
+        <ListGroup>
+          {comment.map((c) => {
+            return (
+              <ListGroup.Item key={c._id}>
+                <strong>{c.author}</strong>
+                <br />
+                {c.comment}
+                <br />
+                Valoración: {c.rate}
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      )}
+    </div>
+  );
+};
 
 export default CommentArea;
--- src/CommentsList.jsx
+++ src/CommentsList.jsx
@@ -1,7 +1,7 @@
 import { ListGroup, Button } from "react-bootstrap";
 const ApiUrl = "https://striveschool-api.herokuapp.com/api/comments/";
 const Token =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWUxZTFhYzczOWY4NzAwMTU3YWIwODgiLCJpYXQiOjE3NzY0MTEwNTIsImV4cCI6MTc3NzYyMDY1Mn0.TlevMKTw6sR2qLolKy_AF0czZscRN7wRkVPwPX79Vq4";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWY5ZDM0MTU0YTMxNTAwMTU1OGIxOTkiLCJpYXQiOjE3Nzc5ODA2MDUsImV4cCI6MTc3OTE5MDIwNX0.T7igyURftk5KrwCoy6S1FnaCumggluThVeSNXjXc-tE";
 
 const CommentsList = ({ comments, setComments }) => {
   const deleteComment = (commentId) => {
@@ -13,13 +13,13 @@ const CommentsList = ({ comments, setComments }) => {
     })
       .then((res) => {
         if (res.ok) {
-          setComments(comments.filter((c) => c._id !== commentId))
+          setComments(comments.filter((c) => c._id !== commentId));
         } else {
-          throw new Error("Errore nella DELETE")
+          throw new Error("Errore nella DELETE");
         }
       })
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
     <ListGroup>
@@ -42,6 +42,6 @@ const CommentsList = ({ comments, setComments }) => {
         </ListGroup.Item>
       ))}
     </ListGroup>
-  )
-}
+  );
+};
 export default CommentsList;
